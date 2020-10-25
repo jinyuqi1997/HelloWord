b444c47cfaff8675303561613a80cdefdff4e751
jinyuqi <969952727@qq.com>
2020-10-25 23:17:30 +0800

    GitHub测试训练！！

--- HelloWord/src/main/java/hello.java
+++ HelloWord/src/main/java/hello.java
@@ -1,5 +1,8 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("靳钰麒  好帅！！");
+        System.out.println("我也赞同");
+        System.out.println("hello Git");
     }
 }
